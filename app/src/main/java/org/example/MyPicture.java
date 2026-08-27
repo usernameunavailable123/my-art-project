733640e62abdc93ee4f171f9e5efe986d6eb6c43
SeanMcEwen1 <smcewen28@ssis.edu.vn>
2026-08-27 09:15:52 +0700

    add ground and added leefs to the flower

--- app/src/main/java/org/example/MyPicture.java
+++ app/src/main/java/org/example/MyPicture.java
@@ -1,40 +1,42 @@
 package org.example;
 
-import java.util.List;
-
-/**
- * MyPicture.java
- * --------------
- * Write the code to draw your scene here. Most of your changes should go inside
- * the drawPicture method below, unless you're defining additional methods or
- * variables to help organize your code.
- *
- * If you want to enhance the functionality of the drawing library itself (e.g. add
- * a new shape function), put that in SimpleGraphics.java instead.
- */
 public class MyPicture {
 
-
     public static void drawPicture(double width, double height) {
+        // Sky
         SimpleGraphics.fillBackground("blue");
+
+        // Ground
+        SimpleGraphics.setFillColor("green");
+        SimpleGraphics.fillRectangle(0, 350, width, 50);
+
+        // Flower stem
         SimpleGraphics.setOutlineColor("green");
+        SimpleGraphics.setLineThickness(5);
         SimpleGraphics.drawStem(200, 225, 200, 350);
+
+        // Leaves
+        SimpleGraphics.setFillColor("green");
+
+        // Left leaf
+        SimpleGraphics.fillOval(150, 275, 50, 25);
+
+        // Right leaf
+        SimpleGraphics.fillOval(200, 290, 50, 25);
+
+        // Flower petals
+        SimpleGraphics.setOutlineColor("green");
         SimpleGraphics.drawPetals(200, 200, 12, 60);
+
         SimpleGraphics.setFillColor("white");
         SimpleGraphics.fillPetals(200, 200, 12, 60);
-        // Fill the background
+
+        // Flower center
         SimpleGraphics.setFillColor("#FFFF00");
         SimpleGraphics.fillCircle(200, 200, 25);
-        //this code creates a circle for the pistil of the flower. 
-
-        
-        
-
-      
     }
 
     public static void main(String[] args) {
-        // Launch the window; only edit the starting canvas dimensions if you'd like to.
         SimpleGraphics.start(MyPicture::drawPicture, 600, 400);
     }
 }
